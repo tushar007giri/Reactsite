e6e0362d23c06f53ae23a2fbbe38814c0f2a64cd
tushar giri <tushargiriexclusive@gmail.com>
2020-12-09 22:35:52 +0530

    first

---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import web from "../src/images/img2.svg";
+import Common from './Common';
+
+const Home = () => {
+    return (
+        <>
+           <Common  name="Grow your business with" imgsrc={web} visit="/service" btname="Get Started"/>   
+        </>
+    )
+
+}
+export default Home;
